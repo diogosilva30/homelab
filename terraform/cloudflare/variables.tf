9b5d3e0942b97b93a38bc5e7ec568084d09ba58a
root <diogo.silva@five9.com>
2024-12-26 15:44:58 +0000

    feat: add terraform code to manage cloudflare

---
+++ terraform/cloudflare/variables.tf
@@ -0,0 +1,20 @@
+variable "cloudflare_api_token" {
+  type        = string
+  sensitive   = true
+  description = "API token to manage cloudflare"
+}
+variable "cloudflare_account_id" {
+  type        = string
+  description = "Account ID of cloudflare"
+  default     = "8f2f7b46178f7faf8e69b9f3ef8282bd"
+}
+variable "domain" {
+  default = "dsilva.dev"
+  type    = string
+}
+variable "emails_for_access_vpn" {
+  type        = list(string)
+  description = "List of emails that can access VPN admin page."
+}
+
+
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
-# Import zitadel package
 module "zitadel" {
   source = "./zitadel"
+}
+module "cloudflare" {
+  source               = "./cloudflare"
+  cloudflare_api_token = var.cloudflare_api_token
+  emails_for_access_vpn = [
+    "diogosilv30@gmail.com",
+  ]
 }
