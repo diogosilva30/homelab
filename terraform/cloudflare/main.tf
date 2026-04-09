c7ee3e82a12c25d616728707ce699a74d13670b9
Diogo Silva <diogo.silva@five9.com>
2026-04-09 13:59:50 +0100

    feat: bump academic editorial to 1.19.0

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -23,6 +23,25 @@ resource "cloudflare_zero_trust_access_policy" "editorial_email_policy" {
   }
 }
 
+resource "cloudflare_zero_trust_access_policy" "editorial_stripe_webhook_bypass_policy" {
+  account_id = var.cloudflare_account_id
+  name       = "The Academic Editorial Stripe webhook bypass policy"
+  decision   = "bypass"
+
+  include {
+    everyone = true
+  }
+}
+
+resource "cloudflare_zero_trust_access_application" "editorial_stripe_webhook_bypass" {
+  zone_id = local.cloudflare_zone_id
+  name    = "The Academic Editorial Stripe webhook bypass"
+  domain  = format("%s.%s/api/payments/stripe/webhook", var.editorial_subdomain, var.domain)
+  policies = [
+    cloudflare_zero_trust_access_policy.editorial_stripe_webhook_bypass_policy.id,
+  ]
+}
+
 resource "cloudflare_zero_trust_access_application" "editorial_protect" {
   zone_id          = local.cloudflare_zone_id
   name             = "The Academic Editorial protection"
