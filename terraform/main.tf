454f03dddc33c07ebd1d510636fc0d1c0d4f1344
Diogo Silva <diogo.silva@five9.com>
2026-05-15 17:40:20 +0100

    feat: add access to brecht@vdvelden.org

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ module "cloudflare" {
     "annehoekman1@gmail.com",
     "bruno.miguel19995@gmail.com",
     "jpmrs1313@gmail.com",
-    "ricardoandradeguimaraes@gmail.com",
-    "tun.nadalini@gmail.com"
+    "tun.nadalini@gmail.com",
+    "brecht@vdvelden.org"
   ]
 }
